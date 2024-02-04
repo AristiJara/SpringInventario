24512a9a87fd88eb3612465f5241b5c0dbf1b2b5
Aristides Jaramillo <aristyjara506@gmail.com>
2024-02-04 15:54:21 -0500

    bakend inventario

---
+++ src/main/java/com/jwt/SpringSecurityJWT/repository/ProductRepo.java
@@ -0,0 +1,7 @@
+package com.jwt.SpringSecurityJWT.repository;
+
+import com.jwt.SpringSecurityJWT.entity.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ProductRepo extends JpaRepository<Product, Integer> {
+}
